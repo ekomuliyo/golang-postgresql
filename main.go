a6ab0a82d92ce5e2d005a0760c810a42e1c2ab18
eko <ekomuliyo@gmail.com>
2020-12-19 21:39:55 +0700

    lagi

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
-import "golang-postgresql/routes"
+import (
+	"golang-postgresql/routes"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 
+	// load env
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// setting port dev / prod
+	var port string
+	if os.Getenv("PORT") == "" {
+		port = ":" + os.Getenv("APP_PORT")
+	} else {
+		port = ":" + os.Getenv("PORT")
+	}
+
 	e := routes.Init()
 
-	e.Logger.Fatal(e.Start(":1234"))
+	e.Logger.Fatal(e.Start(port))
 }
